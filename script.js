125ddf322cc2baf2b3f57d70031f9838101cf97c
Rikke Christiansen <rikkechristiansen@Rikkes-MacBook-Pro.local>
2019-06-15 20:44:02 +0200

    start på javascript

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ window.addEventListener("load", sidenvises)
 
 function sidenvises() {
     console.log("sidenvises");
+    document.querySelector("#mere").addEventListener("click", readMore);
+}
 
+function readMore() {
+    console.log("readMore");
+    document.querySelector("#box").classList.toggle("stor");
+    document.querySelector("#box").classList.toggle("lille");
 }
